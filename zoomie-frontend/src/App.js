4e089d6b385aeb0799368bb221d7beb94390b848
james kwok <kinkwanjames@gmail.com>
2022-12-06 11:27:51 -0500

    set up router

--- zoomie-frontend/src/App.js
+++ zoomie-frontend/src/App.js
@@ -1,3 +1,4 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import './App.scss';
 import BottomNav from './components/BottomNav/BottomNav';
 import Nav from './components/Nav/Nav';
@@ -6,9 +7,14 @@ import HomePage from './pages/HomePage/HomePage';
 function App() {
   return (
     <>
-      <Nav />
-      <HomePage />
-      <BottomNav />
+      <BrowserRouter>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Navigate to="/locations" />} />
+          <Route path="/locations" element={<HomePage />} />
+        </Routes>
+        <BottomNav />
+      </BrowserRouter>
     </>
   );
 }
