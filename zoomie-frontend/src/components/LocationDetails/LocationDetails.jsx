cfe07a8d94eeed4ea27ace0077df44fd772f37e2
james kwok <kinkwanjames@gmail.com>
2022-12-07 00:45:30 -0500

    location details wip

--- zoomie-frontend/src/components/LocationDetails/LocationDetails.jsx
+++ zoomie-frontend/src/components/LocationDetails/LocationDetails.jsx
@@ -1,4 +1,5 @@
 import './LocationDetails.scss';
+import mapPlaceholder from '../../assets/images/placeholder-map.png';
 
 const LocationDetails = ({ location, dog }) => {
   return (
@@ -14,31 +15,41 @@ const LocationDetails = ({ location, dog }) => {
         <div className="LocationDetails__info">
           <div className="LocationDetails__location">
             <div className="LocationDetails__text-container">
+              <h3 className="LocationDetails__title">Location Info</h3>
               <h1 className="LocationDetails__info-title">{location.name}</h1>
               <h2 className="LocationDetails__address">{location.address}</h2>
             </div>
             <div className="LocationDetails__category-label">
               <p className="LocationDetails__category-text">Off-leash</p>
             </div>
-            <button className="LocationDetails__button">
-              <span className="LocationDetails__button-text">Check In</span>
-            </button>
+            <div className="LocationDetails__button-wrapper">
+              <button className="LocationDetails__button">
+                <span className="LocationDetails__button-text">Check In</span>
+              </button>
+            </div>
           </div>
-          <div className="LocationDetails__map">
-            <img className="LocationDetails__map-placeholder" src="" alt="" />
-          </div>
+        </div>
+        <div className="LocationDetails__map">
+          <h3 className="LocationDetails__title">Location Map</h3>
+          <img
+            className="LocationDetails__map-placeholder"
+            src={mapPlaceholder}
+            alt="location-map"
+          />
         </div>
         <div className="LocationDetails__attendance">
           <h2 className="LocationDetails__attendance-title">Who's Going?</h2>
           <div className="LocationDetails__gallery">
-          {dog.map((item, index) => {
+            {dog.map((item, index) => {
               return (
-                <img
-                  className="LocationDetails__avatar"
-                  key={index}
-                  src={item.img}
-                  alt={item.name}
-                />
+                <div className="LocationDetails__profile" key={index}>
+                  <img
+                    className="LocationDetails__avatar"
+                    src={item.img}
+                    alt={item.name}
+                  />
+                  <p className="LocationDetails__dog-name">{item.name}</p>
+                </div>
               );
             })}
           </div>
