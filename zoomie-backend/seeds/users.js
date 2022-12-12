47ace59731444e3326d1aa31dfcb1ba2b182d6f3
james kwok <kinkwanjames@gmail.com>
2022-12-12 16:13:22 -0500

    fix db

--- zoomie-backend/seeds/users.js
+++ zoomie-backend/seeds/users.js
@@ -7,12 +7,32 @@ const hashedPassword = (password) => {
 const usersData = [
   {
     id: 1,
-    email: 'James@example.com',
+    email: '1@example.com',
     password: hashedPassword('password'),
   },
   {
     id: 2,
-    email: 'sharon@example.com',
+    email: '2@example.com',
+    password: hashedPassword('password'),
+  },
+  {
+    id: 3, 
+    email: '3@example.com',
+    password: hashedPassword('password'),
+  },
+  {
+    id: 4,
+    email: '4@example.com',
+    password: hashedPassword('password'),
+  },
+  {
+    id: 5,
+    email: '5@example.com',
+    password: hashedPassword('password'),
+  },
+  {
+    id: 6,
+    email: '6@example.com',
     password: hashedPassword('password'),
   },
 ];
